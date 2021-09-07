1197ca908e92e27cbea79e13b5992d5cb38f247e
asdfgh123456qwe <1090247102@qq.com>
2021-09-07 11:05:22 +0800

    学习内容

---
+++ es6/babel/src/exp.js
@@ -0,0 +1,29 @@
+// let a = 30;
+// export default a;
+
+// let a = "hello";
+// let b = "world";
+// function foo(str){
+//     console.log(str);
+// }
+// export {
+//     a,
+//     b,
+//     foo
+// }
+// export let a = "hello";
+// export let b = "world";
+// export function foo(str){
+//     console.log(str);
+// }
+// var test = "hello";
+
+function foo(){
+    console.log(test);
+}
+// export {
+//     foo
+// }
+module.exports = {
+    foo
+}
