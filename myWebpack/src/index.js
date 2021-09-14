b998c9f4bb08307d91764f67e9e0c272082a3328
asdfgh123456qwe <1090247102@qq.com>
2021-09-15 00:40:28 +0800

    学习内容

--- myWebpack/src/index.js
+++ myWebpack/src/index.js
@@ -1,8 +1,11 @@
 import "./css/index.css";
 import "./css/main.scss";
+import $ from 'jquery';
 console.log("hello world!");
 function foo(){
     console.log("你好");
 }
 foo();
-let arr = [1,2,3,4];
+let arr = [1,2,3,4,5,6];
+
+$(".div2").text("测试");
