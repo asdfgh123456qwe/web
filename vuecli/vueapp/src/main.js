8af972ec509813f9f616eb35ec4909c896590cc7
asdfgh123456qwe <1090247102@qq.com>
2021-10-19 04:00:27 +0800

    学习内容

--- vuecli/vueapp/src/main.js
+++ vuecli/vueapp/src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+import BaiduMap from 'vue-baidu-map'
+
+Vue.use(BaiduMap, {
+  // ak 是在百度地图开发者平台申请的密钥 详见 http://lbsyun.baidu.com/apiconsole/key */
+  ak: 'MkWSCA12mZGer1QAlZGuAP2mOVwqFRm7'
+})
 
 Vue.config.productionTip = false
 Vue.use(ElementUI)
---
+++ vuecli/vueapp/src/views/Page7.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>page7</h1>
+    <baidu-map center="北京" class="bm-view" ak="MkWSCA12mZGer1QAlZGuAP2mOVwqFRm7">
+        <bm-scale anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-scale>
+        <bm-navigation anchor="BMAP_ANCHOR_TOP_RIGHT"></bm-navigation>
+    </baidu-map>
+  </div>
+</template>
+<script>
+// import BaiduMap from "vue-baidu-map/components/map/Map.vue";
+export default {
+//   components: {
+//     BaiduMap,
+//   },
+};
+</script>
+<style scoped>
+.bm-view {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
